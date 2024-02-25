2ed92db2083fcd9edd858650eca67680dc071fb1
saltyypanda <esh7943@rit.edu>
2024-02-24 23:40:35 -0500

    Made navbar fixed at the top, made buttons different colors

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,14 @@
 function Navbar() {
     return (
-        <div className="navbar bg-base-100 h-2">
+        <div className="navbar bg-base-100 h-2 fixed">
             <div className="flex-1">
                 <a href="http://localhost:3000/" className="btn btn-ghost text-xl">Stryde</a>
             </div>
             <div className="flex-none gap-2">
                 <div className="flex-row gap-5 form-control">
-                    <a href="http://localhost:3000/routines"><button class="btn w-36">Routines</button></a>
-                    <a href="http://localhost:3000/workouts"><button class="btn w-36">Workouts</button></a>
-                    <a href="http://localhost:3000/exercises"><button class="btn w-36">Exercises</button></a>
+                    <a href="http://localhost:3000/routines"><button class="btn btn-outline btn-primary w-36">Routines</button></a>
+                    <a href="http://localhost:3000/workouts"><button class="btn btn-outline btn-secondary w-36">Workouts</button></a>
+                    <a href="http://localhost:3000/exercises"><button class="btn btn-outline btn-accent w-36">Exercises</button></a>
                 </div>
                 <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-4">
