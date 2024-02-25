fd5fcc8fe09223b099c36e66181653a05e473610
saltyypanda <esh7943@rit.edu>
2024-02-24 20:56:35 -0500

    Implemented default pages

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <>
+            <p>login unimplemented</p>
+        </>
+    );
+}
+
+export default Login;
---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,9 @@
+function Profile() {
+    return (
+        <>
+            <p>profile unimplemented</p>
+        </>
+    );
+}
+
+export default Profile;
